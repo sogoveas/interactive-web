24b03a40a3a1e511fd7fe2a1cdc9bf6b968cbdb5
Sonia Goveas <sogoveas@gmail.com>
2026-05-14 11:58:00 -0700

    update

--- js/jam-spread.js
+++ js/jam-spread.js
@@ -1,12 +1,13 @@
 let img;
 let knife;
-let trail = [];
+let strokes = [];
+let currentStroke = [];
 let font;
 
 function preload() {
   img = loadImage("images/jambo.png");
   knife = loadImage("images/butterknife.png");
-  font = loadFont("fonts/Mynerve-Regular.ttf"); 
+  font = loadFont("fonts/Mynerve-Regular.ttf");
 }
 
 function setup() {
@@ -14,7 +15,18 @@ function setup() {
   imageMode(CENTER);
   textFont(font);
   noCursor();
-  background('#FDF2F2'); 
+  background('#FDF2F2');
+}
+
+function mousePressed() {
+  currentStroke = [];
+}
+
+function mouseReleased() {
+  if (currentStroke.length > 0) {
+    strokes.push([...currentStroke]);
+    currentStroke = [];
+  }
 }
 
 function draw() {
@@ -27,23 +39,73 @@ function draw() {
   let h = img.height * scaleFactor;
   image(img, width / 2, height / 2, w, h);
 
-  if (mouseIsPressed) {
-    trail.push({ x: mouseX, y: mouseY });
+  if (mouseIsPressed && (mouseX !== pmouseX || mouseY !== pmouseY)) {
+    currentStroke.push({
+      x: mouseX,
+      y: mouseY,
+      px: pmouseX,
+      py: pmouseY,
+      speed: dist(mouseX, mouseY, pmouseX, pmouseY)
+    });
   }
 
-  noFill();
-  stroke('#800606');
-  strokeWeight(14);
-  strokeCap(ROUND);
+  let allStrokes = [...strokes, currentStroke];
 
-  beginShape();
-  for (let i = 0; i < trail.length; i++) {
-    curveVertex(trail[i].x, trail[i].y);
+  for (let s = 0; s < allStrokes.length; s++) {
+    let stroke_ = allStrokes[s];
+
+    for (let i = 0; i < stroke_.length; i++) {
+      let p = stroke_[i];
+
+      let baseWeight = map(i, 0, stroke_.length - 1, 22, 6);
+      let wobble = sin(i * 0.4) * map(p.speed, 0, 30, 0.5, 2.5);
+      let sw = max(2, baseWeight + wobble);
+
+      let r = map(i, 0, stroke_.length - 1, 140, 100);
+      let g = map(i, 0, stroke_.length - 1, 10, 5);
+      let b = map(i, 0, stroke_.length - 1, 20, 40);
+      let a = map(i, 0, stroke_.length - 1, 230, 140);
+
+      strokeCap(ROUND);
+
+      if (i > 1) {
+        let hi_a = map(i, 0, stroke_.length - 1, 80, 0);
+        stroke(200, 60, 70, hi_a);
+        strokeWeight(max(1, sw * 0.25));
+        let prev = stroke_[i - 1];
+        let offX = -(p.y - prev.y) * 0.18;
+        let offY =  (p.x - prev.x) * 0.18;
+        line(prev.x + offX, prev.y + offY, p.x + offX, p.y + offY);
+      }
+
+      stroke(r, g, b, a);
+      strokeWeight(sw);
+      if (i > 0) {
+        let prev = stroke_[i - 1];
+        line(prev.x, prev.y, p.x, p.y);
+      }
+
+      if (i % 9 === 0 && i > 0) {
+        noStroke();
+        fill(80, 15, 10, map(i, 0, stroke_.length - 1, 180, 80));
+        let seedOffX = random(-sw * 0.4, sw * 0.4);
+        let seedOffY = random(-sw * 0.4, sw * 0.4);
+        ellipse(p.x + seedOffX, p.y + seedOffY, random(2, 4), random(3, 5));
+      }
+    }
+
+    if (stroke_.length > 4) {
+      let tip = stroke_[stroke_.length - 1];
+      stroke(110, 8, 18, 180);
+      strokeWeight(4);
+      strokeCap(ROUND);
+      let dripLen = map(stroke_.length, 0, 200, 0, 28);
+      line(tip.x, tip.y, tip.x + random(-1, 1), tip.y + dripLen);
+    }
   }
-  endShape();
 
-  fill('#3933FA');
   noStroke();
+  fill('#3933FA');
   textSize(65);
   textAlign(LEFT, TOP);
   text("repurpose me", 20, 20);
@@ -56,4 +118,5 @@ function draw() {
 
 function windowResized() {
   resizeCanvas(windowWidth, windowHeight);
+  background('#FDF2F2');
 }
